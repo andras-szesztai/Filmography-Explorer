3aaf2fd50e9cdca6b6d199caac2583737f606bd8
andras-szesztai <andras@newzoo.com>
2020-06-20 11:41:05 +0200

    removed global css and added Global styles

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import Helmet from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
+import { Global, css } from '@emotion/core'
 
-import 'modern-normalize'
-import '../../styles/main.css'
+// import 'modern-normalize'
+// import '../../styles/main.css'
 
 const Layout: React.FC = ({ children }) => {
   const {
@@ -30,6 +31,27 @@ const Layout: React.FC = ({ children }) => {
       <Helmet
         title={siteMetadata.title}
         meta={[{ name: 'description', content: siteMetadata.description }, { name: 'keywords', content: siteMetadata.keywords }]}
+      />
+      <Global
+        styles={css`
+          @import url('https://use.typekit.net/sds6vgg.css');
+          html {
+            box-sizing: border-box;
+          }
+
+          *,
+          *::before,
+          *::after {
+            box-sizing: inherit;
+          }
+
+          html {
+            font-family: gill-sans-nova, sans-serif;
+            font-weight: 300;
+            font-style: normal;
+            font-size: 8px;
+          }
+        `}
       />
       {children}
     </>
